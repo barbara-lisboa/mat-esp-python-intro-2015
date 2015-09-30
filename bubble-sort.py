0e4328bdd71ab312ba2a63cae1c0ea8febdef368
barbara-lisboa <gbmmarine@gmail.com>
2015-09-30 19:56:19 -0300

    adição de comentários nas linhas restantes

--- bubble-sort.py
+++ bubble-sort.py
@@ -4,35 +4,35 @@ lista = [11,18,3,1,16,12,6,19,5,0,14,4,17,9,13,7,10,15,2,8] #lista de variaveis 
 
 #gráfico figura final (lista desorganizada)
 
-plt.figure() 
-plt.plot(range(0, N),lista,'ok')
-plt.title("Estado Inicial")
-plt.xlabel("Indice")
-plt.ylabel("Valor")
-plt.savefig("fig/bubble-inicio.png")
-plt.close()
+plt.figure() #cria figura
+plt.plot(range(0, N),lista,'ok') #propriedades do grafico: alcance, variavel utilizada, grafico de bolas pretas
+plt.title("Estado Inicial") #adiciona titulo 
+plt.xlabel("Indice") #nomeia o eixo x
+plt.ylabel("Valor") #nomeia o eixo y
+plt.savefig("fig/bubble-inicio.png") #nomeia os arquivos criados e os coloca na pasta fig
+plt.close() #termina a criacao dos graficos
 
-troca = 0
-iteracao = 0
+troca = 0 #cria uma variavel troca
+iteracao = 0 #cria uma variavel iteracao
 print("lista original", lista) #python executa a lista original
 for i in range(0, N-1, 1): #para gerar a lista i usa-se o comando range, onde os dois primeiros valores representam o começo e o fim da lista, e o terceiro representa o intervalo entre eles
     for j in range(i+1, N, 1): #para gerar a lista j usa-se o comanado range, repitindo o processo anterior para determinar a ordem
-        iteracao = iteracao + 1
-        plt.figure()
+        iteracao = iteracao + 1 #faz com que iteracao seja contada corretamente
+        plt.figure() #linhas 21 a 29 possuem as mesmas propriedades do grafico feito anteriormente
         plt.plot(range(0, N, 1),lista,'ok')
-        plt.plot(i,lista [i],'or')
-        plt.plot(j,lista [j],'ob')
+        plt.plot(i,lista [i],'or') #grafico de bolas vermelhas
+        plt.plot(j,lista [j],'ob') #grafico de bolas azuis
         plt.title("Iteracao {}".format(iteracao))
         plt.xlabel("Indice")
         plt.ylabel("Valor")
-        plt.savefig("fig/bubble-it-{}.png".format(iteracao))
+        plt.savefig("fig/bubble-it-{}.png".format(iteracao)) #{} e .format servem para variar iteracao dentro do {}
         plt.close()
         if lista [i] > lista [j]: #caso a i seja maior que j, aplica-se os proximos codigos, caso seja menor, pega-se o proximo numero
             temp = lista [i]  #temp funciona como uma terceira variavel, que é igual a variavel i
             lista [i] = lista [j] #i é igual a j, e temp continua sendo igual a i antes de ser igual a j
             lista [j] = temp #j troca de lugar com i
             troca = troca + 1
-            plt.figure()
+            plt.figure() #linhas 35 a 41 possuem as mesmas propriedades dos graficos feitos anteriormente
             plt.plot(range(0, N),lista,'ok')
             plt.title("Estado Transitorio")
             plt.xlabel("Indice")
@@ -41,13 +41,13 @@ for i in range(0, N-1, 1): #para gerar a lista i usa-se o comando range, onde os
             plt.close()
             
 print("lista em ordem crescente", lista) #python executa a lista original em ordem crescente 
-lista [0:5] 
-lista [N:N-6:-1]
-print("cinco maiores valores", lista [N:N-6:-1])
-print("cinco menores valores", lista [0:5])
+lista [0:5] #pega os 5 primeiros valores da lista
+lista [N:N-6:-1] #pega os 5 ultimos valores da lista
+print("cinco maiores valores", lista [N:N-6:-1]) #escreve os cinco maiores valores 
+print("cinco menores valores", lista [0:5]) #escreve os cinco menores valores
 
 #gráfico figura inicial (lista organizada)
-plt.figure()
+plt.figure() #linhas 50 a 56 possuem as mesmas propriedades do grafico feito anteriormente
 plt.plot(range(0, N),lista,'ok')
 plt.title("Estado Final")
 plt.xlabel("Indice")
